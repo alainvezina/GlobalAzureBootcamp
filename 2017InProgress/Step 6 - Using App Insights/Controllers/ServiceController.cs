a40365fef93480a7b725ffcd86f0a28ff581692f
Samir Bellouti <Samir.Bellouti@transat.com>
2017-04-14 18:08:27 -0400

    Added code snippets and cs files

---
+++ 2017InProgress/Step 6 - Using App Insights/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+namespace Step1WebApp.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            try
+            {
+                Services.SomeService.SomeWorkWithDependency();
+            }
+            catch (Exception ex)
+            {
+                var client = new TelemetryClient();
+                client.TrackException(ex);
+            }
+            return View();
+
+
+        }
+    }
+}
